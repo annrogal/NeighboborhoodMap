79ee4713f837ad22dd211b387266b8a2d4b9dc0c
Anna Rogal <annrogal1@gmail.com>
2018-08-14 19:09:44 +0200

    change map load

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
-import {withGoogleMap, GoogleMap} from 'react-google-maps';
+
 class Map extends Component {
-    render() {
-        const GoogleMapExample = withGoogleMap(props => (
-        <GoogleMap 
-            defaultCenter={{lat: 54.506207, lng: 18.538278}}
-            defaultZoom={13}>
-        </GoogleMap>
-        ));
-        
+
+    componentDidMount() {
+        window.initMap = this.initMap;
+        loadJS("https://maps.googleapis.com/maps/api/js?key=AIzaSyBTy_Dyw1ErxbzVbY_S2w8Je1GgdearAuI&callback=initMap")
+    }
+
+    initMap() {
+        const mapDiv = document.getElementById('map');
+        let map = new window.google.maps.Map(mapDiv, {
+            center: {lat: 54.506207, lng: 18.538278},
+            zoom: 13
+        })
+    }
+
+    render() {        
         return (
-            <div id="map">
-                <GoogleMapExample
-                    containerElement={ <div style={{ height: `1000px`, width: '100%' }} /> }
-                    mapElement={ <div style={{ height: `100%` }} /> }
-                />
-            </div>
+            <div ref="map" id="map" role="application"/>
         )
     }
 }
 
 export default Map;
+
+function loadJS(src) {
+    const ref = window.document.getElementsByTagName("script")[0];
+    const script = window.document.createElement("script");
+    console.log(script);
+    script.src = src;
+    script.async = true;
+    script.defer = true;
+    ref.parentNode.insertBefore(script, ref);
+    console.log(ref.parentNode)
+}
