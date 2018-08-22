2d1e6e06b2c70561243db07ec03c15621d354cf5
Anna Rogal <annrogal1@gmail.com>
2018-08-22 17:05:28 +0200

    fix problem with infowindow close

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
+import {locations} from './location'
 
 class Map extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            locations: locations, 
+            map: '', 
+            markers: [], 
+            infoWindow: '',
+            previousMarker: ''
+        }
+        this.initMap = this.initMap.bind(this);
+        this.openInfoWindow = this.openInfoWindow.bind(this);
+        this.closeInfoWindow = this.closeInfoWindow.bind(this);
+    }
 
     componentDidMount() {
         window.initMap = this.initMap;
@@ -9,10 +23,73 @@ class Map extends Component {
 
     initMap() {
         const mapDiv = document.getElementById('map');
+        let infoWindow = new window.google.maps.InfoWindow({});
+        let bounds = new window.google.maps.LatLngBounds();
+
         let map = new window.google.maps.Map(mapDiv, {
-            center: {lat: 54.506207, lng: 18.538278},
-            zoom: 13
+            center: {lat: 54.520498, lng: 18.539023},
+            zoom: 15
         })
+
+        this.setState({
+            map: map,
+            infoWindow: infoWindow
+        })
+
+        this.state.locations.map(location => {
+            let marker = new window.google.maps.Marker({
+                position: new window.google.maps.LatLng(location.lat, location.lng),
+                map: map,
+                title: location.title,
+                address: location.address,
+                id: location
+            });
+
+            this.state.markers.push(marker);
+
+           
+            
+            marker.addListener('click', () => {
+                    
+                this.openInfoWindow(marker);
+
+                bounds.extend(marker.position);
+                
+
+                map.fitBounds(bounds);
+                
+            });
+        })        
+    }
+
+    openInfoWindow(marker) {
+        marker.infoWindow = new window.google.maps.InfoWindow({});
+
+        this.closeInfoWindow();
+
+        this.state.infoWindow.open(this.state.map, marker);
+                
+                this.state.infoWindow.setContent(`${marker.title}</br>address: ${marker.address}</br>${marker.position}`);
+
+                this.state.infoWindow.addListener('closeclick', () => {
+                    this.state.infoWindow.setMarker = null;
+                });
+
+                this.setState({
+                    previousMarker: marker
+                })
+
+    }
+
+    closeInfoWindow() {
+        if(this.state.previousMarker) {
+            this.state.previousMarker.setAnimation(null);
+        }
+        this.setState({
+            previousMarker: ''
+        });
+
+        this.state.infoWindow.close();
     }
 
     render() {        
---
+++ src/location.js
@@ -0,0 +1,34 @@
+const locations = [
+    {
+        title: 'Tawerna Gdynia',
+        lat: 54.518750,
+        lng: 18.548094,
+        address: 'al. Jana Pawła II 11'
+    },
+    {
+        title: 'Sphinx',
+        lat: 54.521903,
+        lng: 18.539485,
+        address: '10 Lutego 11'
+    },
+    {
+        title: 'tratoria La Fortuna',
+        lat: 54.519709,
+        lng: 18.542056,
+        address: 'Armii Krajowej 9'
+    },
+    {
+        title: 'Pueblo',
+        lat: 54.518814,
+        lng: 18.539307,
+        address: 'Antoniego Abrahama 56'
+    },
+    {
+        title: 'Lolo Thai Jolo',
+        lat: 54.524562,
+        lng: 18.543171,
+        address: 'Jana z Kolna 2'
+    }
+]
+
+export {locations}
