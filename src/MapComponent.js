35f2fcbf08d3bfbfa3e0ab774772b1b4e3addc8f
Anna Rogal <annrogal1@gmail.com>
2018-08-23 18:23:42 +0200

    add list and query

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
 import {locations} from './location'
+import escapeRegExp from 'escape-string-regexp'
 
 class Map extends Component {
     constructor(props) {
         super(props);
         this.state = { 
             locations: locations, 
-            map: '', 
-            markers: [], 
+            map: '',  
             infoWindow: '',
-            previousMarker: ''
+            previousMarker: '',
+            markers: [],
+            filterLocations: {},
+            query: ''
         }
         this.initMap = this.initMap.bind(this);
         this.openInfoWindow = this.openInfoWindow.bind(this);
         this.closeInfoWindow = this.closeInfoWindow.bind(this);
+        this.filter = this.filter.bind(this);
+        this.updateQuery = this.updateQuery.bind(this);
+        this.resetQuery = this.resetQuery.bind(this);
     }
 
     componentDidMount() {
         window.initMap = this.initMap;
         loadJS("https://maps.googleapis.com/maps/api/js?key=AIzaSyBTy_Dyw1ErxbzVbY_S2w8Je1GgdearAuI&callback=initMap")
+    }
+
+    updateQuery = (query) => {
+        this.setState({query: query.trim()})
+    }
+
+    resetQuery = () => {
+        this.setState({query: ''})
     }
 
     initMap() {
@@ -36,30 +50,32 @@ class Map extends Component {
             infoWindow: infoWindow
         })
 
-        this.state.locations.map(location => {
+        this.state.filterLocations.map(location => {
             let marker = new window.google.maps.Marker({
                 position: new window.google.maps.LatLng(location.lat, location.lng),
                 map: map,
                 title: location.title,
                 address: location.address,
-                id: location
+                id: location.id
+            });
+           
+            marker.addListener('click', () => {
+                this.openInfoWindow(marker);
+                bounds.extend(marker.position);
+                map.fitBounds(bounds);
             });
 
             this.state.markers.push(marker);
-
-           
-            
-            marker.addListener('click', () => {
-                    
-                this.openInfoWindow(marker);
-
-                bounds.extend(marker.position);
-                
-
-                map.fitBounds(bounds);
-                
-            });
         })        
+    }
+
+    filter(query) {
+       if(query) {
+        const match = new RegExp(escapeRegExp(query), 'i');
+        this.state.filterLocations = locations.filter(location => match.test(location.title))
+       }else{
+           this.state.filterLocations = locations
+       }
     }
 
     openInfoWindow(marker) {
@@ -78,13 +94,9 @@ class Map extends Component {
                 this.setState({
                     previousMarker: marker
                 })
-
     }
 
     closeInfoWindow() {
-        if(this.state.previousMarker) {
-            this.state.previousMarker.setAnimation(null);
-        }
         this.setState({
             previousMarker: ''
         });
@@ -93,8 +105,34 @@ class Map extends Component {
     }
 
     render() {        
+        const {query} = this.state
+
+        this.filter(query);
+
+const locationList = this.state.filterLocations.map(location => {
+    return (
+        <li key={location.id} id={location.id} name={location.title}>{location.title}</li>
+    )
+})
+
         return (
-            <div ref="map" id="map" role="application"/>
+            <div>
+            <div ref="map" id="map" role="application" />
+            <div className='search-bar'>
+                <div className='search-input'>
+                    <input type='text' placeholder='Search restaurant'
+                    aria-label='Search restaurants'
+                    role="search"
+                    tabIndex='1'
+                    value={query}
+                    onChange={(event) => this.updateQuery(event.target.value)}               
+                   />
+                    <ul className='location-list' aria-label='List of restaurants'>
+                        {locationList}
+                    </ul>
+                </div>
+            </div>
+            </div>
         )
     }
 }
--- src/location.js
+++ src/location.js
@@ -3,31 +3,35 @@ const locations = [
         title: 'Tawerna Gdynia',
         lat: 54.518750,
         lng: 18.548094,
-        address: 'al. Jana Pawła II 11'
+        address: 'al. Jana Pawła II 11',
+        id: 1
     },
     {
         title: 'Sphinx',
         lat: 54.521903,
         lng: 18.539485,
-        address: '10 Lutego 11'
+        address: '10 Lutego 11',
+        id: 2
     },
     {
         title: 'tratoria La Fortuna',
         lat: 54.519709,
         lng: 18.542056,
-        address: 'Armii Krajowej 9'
+        address: 'Armii Krajowej 9',
+        id: 3
     },
     {
         title: 'Pueblo',
         lat: 54.518814,
         lng: 18.539307,
-        address: 'Antoniego Abrahama 56'
-    },
+        address: 'Antoniego Abrahama 56',
+        id: 4  },
     {
         title: 'Lolo Thai Jolo',
         lat: 54.524562,
         lng: 18.543171,
-        address: 'Jana z Kolna 2'
+        address: 'Jana z Kolna 2',
+        id: 5
     }
 ]
 
